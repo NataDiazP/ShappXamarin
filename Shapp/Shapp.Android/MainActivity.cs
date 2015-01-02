29efc3c2226073d9fd13d69660a6a2304a7b584f
natad <julydiazp@hotmail.com>
2015-01-02 10:49:25 -0500

    holi

---
+++ Shapp/Shapp.Android/MainActivity.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+using Xamarin.Forms.Platform.Android;
+using Android.Graphics.Drawables;
+using Android.Graphics;
+
+namespace Shapp.Droid
+{
+    [Activity(Label = "Shapp", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            RequestWindowFeature(WindowFeatures.NoTitle);
+
+            SetContentView(Resource.Layout.holi);
+        }
+    }
+}
+
---
+++ Shapp/Shapp/MainPage.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace Shapp
+{
+    class MainPage : ContentPage
+    {
+        public MainPage() {
+            Button reg = new Button { 
+                Text = "registrate",
+                BackgroundColor = Color.FromHex("be1e59"),
+                TextColor = Color.White,
+                HorizontalOptions = LayoutOptions.Start,
+                BorderRadius = 20,
+                WidthRequest = 75
+            };
+
+            reg.Clicked += (sender, e) => {
+                DisplayAlert("Holi", "Esto es un saludito", "Ok", "Cancel");
+            };
+            
+
+
+            Content = new StackLayout
+            {
+                Spacing = 2,
+                VerticalOptions = LayoutOptions.Fill,
+                BackgroundColor = Color.White,
+                MinimumWidthRequest = 100,
+                Children = { 
+                    new Label {
+                        Text = "Bienvenido a",
+                        HorizontalOptions = LayoutOptions.Center,
+                        TextColor = Color.Black
+                    },
+                    new Image{
+                        Aspect = Aspect.AspectFit,
+                        HorizontalOptions = LayoutOptions.Center,
+                        Source =  Device.OnPlatform(
+                            iOS: ImageSource.FromFile("Images/logo.png"),
+                            Android:  ImageSource.FromFile("logo.png"),
+                            WinPhone: ImageSource.FromFile("Images/logo.png"))
+                    },
+                    new Image{
+                        Aspect = Aspect.AspectFit,
+                        HorizontalOptions = LayoutOptions.Center,
+                        Source =  Device.OnPlatform(
+                            iOS: ImageSource.FromFile("Images/nombre.png"),
+                            Android:  ImageSource.FromFile("nombre.png"),
+                            WinPhone: ImageSource.FromFile("Images/nombre.png"))
+                    },
+                    new Label {
+                        Text = "Deseos a tu alcance",
+                        TextColor = Color.FromHex("be1e59"),
+                        HorizontalOptions = LayoutOptions.Center
+                    },
+                    new StackLayout{
+                        VerticalOptions = LayoutOptions.Fill,
+                        Spacing = 1,
+                        WidthRequest = 5,
+                        HorizontalOptions = LayoutOptions.Start,
+                        Children = {
+                            reg
+                        }
+                    }
+                }
+            };
+        }
+    }
+}
